83b34abec9535af13acfb2cbcea6bd0d14e3f0d3
Jesse Gibson <JesseTheGibson@gmail.com>
2023-01-02 20:56:24 -0500

    Exclude dependencies from published build
    Vite/Rollup really wants to bundle my dependencies. I've generated
    a bundle blacklist for my production dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
 import { defineConfig } from 'vite';
-import { builtinModules } from 'module';
+import { readFile } from 'fs/promises';
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: './src/index.ts',
-      name: 'unstated',
-      fileName: 'unstated',
+export default defineConfig(async () => {
+  const pkg = await readFile('package.json', 'utf8');
+  const { dependencies, peerDependencies } = JSON.parse(pkg);
+
+  return {
+    build: {
+      lib: {
+        entry: './src/index.ts',
+        name: 'unstated',
+        fileName: 'unstated',
+      },
+      rollupOptions: {
+        external: [dependencies, peerDependencies].map(Object.keys).flat(),
+
+        // Only used for UMD builds.
+        output: {
+          globals: {
+            react: 'React',
+            immer: 'immer',
+          },
+        },
+      },
     },
-    rollupOptions: {
-      // TODO: Dynamically exclude all production dependencies.
-      external: builtinModules,
-    },
-  },
+  };
 });
